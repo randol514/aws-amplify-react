134a5ecd558498e886a6afd6b8a475d6cfdf8590
Randol Paul Huaman Allcca <randol.paul2015@gmail.com>
2025-05-21 18:25:15 -0500

    add component & modified App

---
+++ src/components/Component.jsx
@@ -0,0 +1,10 @@
+const Component = () =>{
+  return(
+    <>
+    <h4>This is a component</h4>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, maxime?</p>
+    </>
+  )
+}
+
+export default Component
